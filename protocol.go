4b758d8bfa8052790e0cea9646cc8405ad004b6e
vishal.chauhan <vishal.chauhan@netcorecloud.com>
2021-06-07 18:15:42 +0530

    updates

--- protocol.go
+++ protocol.go
@@ -2,41 +2,13 @@ package proxyproto
 
 import (
 	"bufio"
-	"bytes"
-	"errors"
-	"fmt"
-	"io"
 	"log"
 	"net"
-	"strconv"
-	"strings"
 	"sync"
 	"time"
+
+	proto "github.com/pires/go-proxyproto"
 )
-
-var (
-	// prefix is the string we look for at the start of a connection
-	// to check if this connection is using the proxy protocol
-	prefix    = []byte("PROXY ")
-	prefixLen = len(prefix)
-
-	ErrInvalidUpstream = errors.New("upstream connection address not trusted for PROXY information")
-)
-
-// SourceChecker can be used to decide whether to trust the PROXY info or pass
-// the original connection address through. If set, the connecting address is
-// passed in as an argument. If the function returns an error due to the source
-// being disallowed, it should return ErrInvalidUpstream.
-//
-// If error is not nil, the call to Accept() will fail. If the reason for
-// triggering this failure is due to a disallowed source, it should return
-// ErrInvalidUpstream.
-//
-// If bool is true, the PROXY-set address is used.
-//
-// If bool is false, the connection's remote address is used, rather than the
-// address claimed in the PROXY info.
-type SourceChecker func(net.Addr) (bool, error)
 
 // Listener is used to wrap an underlying listener,
 // whose connections may be using the HAProxy Proxy Protocol (version 1).
@@ -48,8 +20,6 @@ type SourceChecker func(net.Addr) (bool, error)
 type Listener struct {
 	Listener           net.Listener
 	ProxyHeaderTimeout time.Duration
-	SourceCheck        SourceChecker
-	UnknownOK          bool // allow PROXY UNKNOWN
 }
 
 // Conn is used to wrap and underlying connection which
@@ -58,41 +28,19 @@ type Listener struct {
 type Conn struct {
 	bufReader          *bufio.Reader
 	conn               net.Conn
-	dstAddr            *net.TCPAddr
-	srcAddr            *net.TCPAddr
-	useConnAddr        bool
+	header             *proto.Header
 	once               sync.Once
 	proxyHeaderTimeout time.Duration
-	unknownOK          bool
 }
 
 // Accept waits for and returns the next connection to the listener.
 func (p *Listener) Accept() (net.Conn, error) {
 	// Get the underlying connection
-	for {
-		conn, err := p.Listener.Accept()
-		if err != nil {
-			return nil, err
-		}
-		var useConnAddr bool
-		if p.SourceCheck != nil {
-			allowed, err := p.SourceCheck(conn.RemoteAddr())
-			if err != nil {
-				if err == ErrInvalidUpstream {
-					conn.Close()
-					continue
-				}
-				return nil, err
-			}
-			if !allowed {
-				useConnAddr = true
-			}
-		}
-		newConn := NewConn(conn, p.ProxyHeaderTimeout)
-		newConn.useConnAddr = useConnAddr
-		newConn.unknownOK = p.UnknownOK
-		return newConn, nil
+	conn, err := p.Listener.Accept()
+	if err != nil {
+		return nil, err
 	}
+	return NewConn(conn, p.ProxyHeaderTimeout), nil
 }
 
 // Close closes the underlying listener.
@@ -121,27 +69,16 @@ func NewConn(conn net.Conn, timeout time.Duration) *Conn {
 // it is returned and the socket is closed.
 func (p *Conn) Read(b []byte) (int, error) {
 	var err error
-	p.once.Do(func() { err = p.checkPrefix() })
+	p.once.Do(func() { err = p.checkHeader() })
 	if err != nil {
-		return 0, err
+		// If no proxy protocol header is present, the connection is still valid.
+		if err == proto.ErrNoProxyProtocol {
+			log.Printf("[WARN] Failed to read proxy protocol header: %v", err)
+		} else {
+			return 0, err
+		}
 	}
 	return p.bufReader.Read(b)
-}
-
-func (p *Conn) ReadFrom(r io.Reader) (int64, error) {
-	if rf, ok := p.conn.(io.ReaderFrom); ok {
-		return rf.ReadFrom(r)
-	}
-	return io.Copy(p.conn, r)
-}
-
-func (p *Conn) WriteTo(w io.Writer) (int64, error) {
-	var err error
-	p.once.Do(func() { err = p.checkPrefix() })
-	if err != nil {
-		return 0, err
-	}
-	return p.bufReader.WriteTo(w)
 }
 
 func (p *Conn) Write(b []byte) (int, error) {
@@ -153,10 +90,6 @@ func (p *Conn) Close() error {
 }
 
 func (p *Conn) LocalAddr() net.Addr {
-	p.checkPrefixOnce()
-	if p.dstAddr != nil && !p.useConnAddr {
-		return p.dstAddr
-	}
 	return p.conn.LocalAddr()
 }
 
@@ -168,9 +101,25 @@ func (p *Conn) LocalAddr() net.Addr {
 // client is slow. Using a Deadline is recommended if this is called
 // before Read()
 func (p *Conn) RemoteAddr() net.Addr {
-	p.checkPrefixOnce()
-	if p.srcAddr != nil && !p.useConnAddr {
-		return p.srcAddr
+	p.once.Do(func() {
+		if err := p.checkHeader(); err != nil && err != proto.ErrNoProxyProtocol {
+			log.Printf("[ERR] Failed to read proxy prefix: %v", err)
+			p.Close()
+			p.bufReader = bufio.NewReader(p.conn)
+		}
+	})
+	if p.header != nil && p.header.Command.IsProxy() {
+		if p.header.TransportProtocol.IsStream() {
+			return &net.TCPAddr{
+				IP:   p.header.SourceAddress,
+				Port: int(p.header.SourcePort),
+			}
+		} else if p.header.TransportProtocol.IsDatagram() {
+			return &net.UDPAddr{
+				IP:   p.header.SourceAddress,
+				Port: int(p.header.SourcePort),
+			}
+		}
 	}
 	return p.conn.RemoteAddr()
 }
@@ -187,104 +136,19 @@ func (p *Conn) SetWriteDeadline(t time.Time) error {
 	return p.conn.SetWriteDeadline(t)
 }
 
-func (p *Conn) checkPrefixOnce() {
-	p.once.Do(func() {
-		if err := p.checkPrefix(); err != nil && err != io.EOF {
-			log.Printf("[ERR] Failed to read proxy prefix: %v", err)
-			p.Close()
-			p.bufReader = bufio.NewReader(p.conn)
-		}
-	})
-}
-
-func (p *Conn) checkPrefix() error {
+func (p *Conn) checkHeader() (err error) {
 	if p.proxyHeaderTimeout != 0 {
 		readDeadLine := time.Now().Add(p.proxyHeaderTimeout)
 		p.conn.SetReadDeadline(readDeadLine)
 		defer p.conn.SetReadDeadline(time.Time{})
 	}
 
-	// Incrementally check each byte of the prefix
-	for i := 1; i <= prefixLen; i++ {
-		inp, err := p.bufReader.Peek(i)
-
-		if err != nil {
-			if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
-				return nil
-			} else {
-				return err
-			}
-		}
-
-		// Check for a prefix mis-match, quit early
-		if !bytes.Equal(inp, prefix[:i]) {
-			return nil
-		}
+	// TODO golden hammer against blocking forever
+	if p.proxyHeaderTimeout == 0 {
+		p.proxyHeaderTimeout = 50 * time.Millisecond
 	}
 
-	// Read the header line
-	header, err := p.bufReader.ReadString('\n')
-	if err != nil {
-		p.conn.Close()
-		return err
-	}
+	p.header, err = proto.ReadTimeout(p.bufReader, p.proxyHeaderTimeout)
 
-	// Strip the carriage return and new line
-	header = header[:len(header)-2]
-
-	// Split on spaces, should be (PROXY <type> <src addr> <dst addr> <src port> <dst port>)
-	parts := strings.Split(header, " ")
-	if len(parts) < 2 {
-		p.conn.Close()
-		return fmt.Errorf("Invalid header line: %s", header)
-	}
-
-	// Verify the type is known
-	switch parts[1] {
-	case "UNKNOWN":
-		if !p.unknownOK || len(parts) != 2 {
-			p.conn.Close()
-			return fmt.Errorf("Invalid UNKNOWN header line: %s", header)
-		}
-		p.useConnAddr = true
-		return nil
-	case "TCP4":
-	case "TCP6":
-	default:
-		p.conn.Close()
-		return fmt.Errorf("Unhandled address type: %s", parts[1])
-	}
-
-	if len(parts) != 6 {
-		p.conn.Close()
-		return fmt.Errorf("Invalid header line: %s", header)
-	}
-
-	// Parse out the source address
-	ip := net.ParseIP(parts[2])
-	if ip == nil {
-		p.conn.Close()
-		return fmt.Errorf("Invalid source ip: %s", parts[2])
-	}
-	port, err := strconv.Atoi(parts[4])
-	if err != nil {
-		p.conn.Close()
-		return fmt.Errorf("Invalid source port: %s", parts[4])
-	}
-	p.srcAddr = &net.TCPAddr{IP: ip, Port: port}
-
-	// Parse out the destination address
-	ip = net.ParseIP(parts[3])
-	if ip == nil {
-		p.conn.Close()
-		return fmt.Errorf("Invalid destination ip: %s", parts[3])
-	}
-	port, err = strconv.Atoi(parts[5])
-	if err != nil {
-		p.conn.Close()
-		return fmt.Errorf("Invalid destination port: %s", parts[5])
-	}
-	p.dstAddr = &net.TCPAddr{IP: ip, Port: port}
-
-	return nil
+	return
 }
